819b0a1ee1ab2a17e22b385c27ceaf21297bcfac
Filipe Silva <filipematossilva@gmail.com>
2016-09-14 05:20:23 +0100

    use app.component from aot-library

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,10 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent }  from './app.component';
+import { LibraryModule, AppComponent } from 'aot-library';
 
 @NgModule({
-  imports: [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports: [ BrowserModule, LibraryModule ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
--- systemjs.config.js
+++ systemjs.config.js
@@ -26,6 +26,7 @@
       // other libraries
       'rxjs':                       'npm:rxjs',
       'angular2-in-memory-web-api': 'npm:angular2-in-memory-web-api',
+      'aot-library': 'npm:aot-library',
     },
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
@@ -38,6 +39,10 @@
       },
       'angular2-in-memory-web-api': {
         main: './index.js',
+        defaultExtension: 'js'
+      },
+      'aot-library': {
+        main: './dist/index.js',
         defaultExtension: 'js'
       }
     }
